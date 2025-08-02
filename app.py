ee7ca0055847a3bc926591c3b3d7ba19f728a04e
NandiniGuduri <nandhiniguduri@gmail.com>
2025-08-02 12:39:40 +0530

    first update

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app=Flask(__name__)
 @app.route("/")
 def index():
-    return "welcome to MRECW"
+    return"welcome to MRECW"
 if __name__=="__main__":
     app.run(host='0.0.0.0',port=10000) 
