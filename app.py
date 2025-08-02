992d1af9eb3555fe9f8756166336a7e667fd2a05
NandiniGuduri <nandhiniguduri@gmail.com>
2025-08-02 14:19:19 +0530

    third update

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template
 app=Flask(__name__)
 @app.route("/")
 def index():
-    return"welcome to MRECW"
+    return render_template("index.html")
+@app.route("/home")
+def home():
+    return "home page"
 if __name__=="__main__":
     app.run(host='0.0.0.0',port=10000)
  
